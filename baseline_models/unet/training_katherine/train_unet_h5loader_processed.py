3ca6592cf251f6f7ad70912aa95dcbba3b060a0c
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-02-10 21:44:36 -0800

    changes to config files and sbatches

--- baseline_models/unet/training_katherine/train_unet_h5loader_processed.py
+++ baseline_models/unet/training_katherine/train_unet_h5loader_processed.py
@@ -221,8 +221,12 @@ def main(cfg: DictConfig) -> float:
         torch.cuda.current_stream().wait_stream(ddps)
 
     # create optimizer
-    if cfg.optimizer == 'adam':
+    if cfg.optimizer == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
+    elif cfg.optimizer == 'AdamW':
+        optimizer = optim.AdamW(model.parameters(), lr=cfg.learning_rate)
+    elif cfg.optimizer == 'RAdam':
+        optimizer = optim.RAdam(model.parameters(), lr=cfg.learning_rate)
     else:
         raise ValueError('Optimizer not implemented')
     
