661a55cddbcf835caf65e8bf0c41cc8526f9b27d
Jerry <jerrylin247365@gmail.com>
2025-03-01 14:41:48 -0800

    refactored preprocessing to be more clear, explicit, and reproducible

--- preprocessing/data_prep_scripts/v2_rh_mc/make_test/create_test_v2_rh_mc.sh
+++ preprocessing/data_prep_scripts/v2_rh_mc/make_test/create_test_v2_rh_mc.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 #SBATCH -A m4334
-#SBATCH -C cpu
+#SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 8:00:00
+#SBATCH -t 40:00
 #SBATCH -n 1
 #SBATCH -c 32
+##SBATCH --gpus-per-task=1
 #SBATCH --image=nvcr.io/nvidia/modulus/modulus:24.01
-#SBATCH --output=create_test_%j.out
 #SBATCH --mail-user=jerryL9@uci.edu
 #SBATCH --mail-type=ALL
+##SBATCH --output=out_%j.out
+##SBATCH --error=eo_%j.err
 
-shifter python create_test_v2_rh_mc.py \
+shifter python ../process_data_split_v2_rh_mc.py \
             'E3SM-MMF.ml2steploc.0009-0[3456789]-*-*.nc' \
             'E3SM-MMF.ml2steploc.0009-1[012]-*-*.nc' \
             'E3SM-MMF.ml2steploc.0010-*-*-*.nc' \
             'E3SM-MMF.ml2steploc.0011-0[12]-*-*.nc' \
-    --save_path '/global/homes/j/jerrylin/scratch/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc_full/test_set/'
+    --data_split 'test' \
+    --stride_sample 12 \
+    --start_idx 1 \
+    --save_h5 True \
+    --save_path '/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc/scoring_set/'
