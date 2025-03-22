611bc5f1e757ef2925d9f57cf179ba87a12be3cc
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-03-21 18:36:55 -0700

    changed script to Q

--- baseline_models/unet/training_katherine/online_eval_short_unet_conf.sh
+++ baseline_models/unet/training_katherine/online_eval_short_unet_conf.sh
@@ -14,7 +14,7 @@ shifter python online_eval_short_no_map.py \
         --mmf_path '/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/mmf_runs/mmf_speedeval_gpu/run/mmf_speedeval_gpu.eam.h2.0003-01-*.nc' \
         --nn_path '/pscratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models/unet_adamW_conf_2/online/unet_conf_1_month/run/unet_conf_1_month.eam.h2.0003-01-*.nc' \
         --save_path '/pscratch/sd/k/kfrields/hugging/scoring/unet_conf_output' \
-        --var 'T' \
+        --var 'Q' \
         --max_day 20 \
         --grid_path '/global/cfs/cdirs/m4334/jerry/climsim3_dev/grid_info/ClimSim_low-res_grid-info.nc' \
         --input_mean_file 'input_mean_v6_pervar.nc' \
